24ac02a21b5982fa6078852b977670a874f8bb39
Lukas-Marx <70298828+Lukas-Marx@users.noreply.github.com>
2021-10-03 00:35:09 +0200

    Upload PDF meta extract

---
+++ Get_PDF_Creation_date.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+import re
+pdf = PdfFileReader('path/data.pdf')
+info = pdf.getDocumentInfo()
+print(info)
+
+create_date = info['/ModDate']#CreationDate if init creation is wanted
+fix = re.search('\d+', create_date)
+
+a = datetime.strptime(fix.group(),'%Y%m%d%H%M%S').date()
+
+print(a)
